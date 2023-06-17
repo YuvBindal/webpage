93f37064a6c371fc4097ea8376a79906cc100782
Yuv Bindal <yuv2bindal@gmail.com>
2023-06-17 20:55:37 +0530

    Added effects like sound, hover, and responsive

--- src/MyComponents/BackgroundTest.js
+++ src/MyComponents/BackgroundTest.js
@@ -1,85 +1,203 @@
 import React from "react";
 import "./Background.css";
 import ImageButton from "./ImageButton";
-
+import { useState } from "react";
 export const BackgroundTest = () => {
-  const handleMediaButton = () => {
-    // Handle button click event
+  const handleGithub = () => {
+    window.location.href = 'https://github.com/YuvBindal'
   };
+  const handleYoutube = () => {
+    window.location.href = 'https://www.youtube.com/@yuvbindal1226'
+  };
+
+  const handleDevpost = () => {
+    window.location.href = 'https://devpost.com/yuv2bindal'
+
+  }
+  const handleLinkedin = () => {
+    window.location.href= 'https://www.linkedin.com/in/yuvbindal/'
+  }
+  const handleInstagram = () => {
+    window.location.href= 'https://www.instagram.com/yuvbindal/'
+  }
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const playSound = () => {
+    setIsHovered(true);
+  };
+
+  const stopSound = () => {
+    setIsHovered(false);
+  };
+
+  
 
   return (
     <>
-      <div class="background">
+      <div className="background">
         <div id="container-stars">
           <div id="stars"></div>
         </div>
         <div id="glow">
-          <div class="circle"></div>
-          <div class="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
         </div>
 
-        <div class="navigationMenu">
-          <strong className="spaceStyle">
-            <a>Home</a>
+        <div className="navigationMenu">
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              Home
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="fast-whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
-          <strong className="spaceStyle">
-            <a>Portfolio</a>
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              Portfolio
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="fast-whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
-          <strong className="spaceStyle">
-            <a>Tech Stack</a>
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              Tech Stack
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="fast-whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
-          <strong className="spaceStyle">
-            <a>Book a mentoring session!</a>
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              Book a mentoring session!
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="fast-whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
-          <strong className="spaceStyle">
-            <a>About BlockState&copy;</a>
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              About BlockState&copy;
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="fast-whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
-          <strong className="spaceStyle">
-            <a>Blog</a>
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              Blog
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
-          <strong className="spaceStyle">
-            <a>Got Projects?</a>
+          <strong
+            onMouseEnter={playSound}
+            onMouseLeave={stopSound}
+            id="homeButton"
+            className="spaceStyle"
+          >
+            <a>
+              Got projects?
+              {isHovered && (
+                <audio autoPlay>
+                  <source src="fast-whoosh.mp3" type="audio/mpeg" />{" "}
+                  {/* Replace "hover-sound.mp3" with the path to your audio file */}
+                </audio>
+              )}
+            </a>
           </strong>
+
         </div>
 
-        <div class="mediaStyle">
+        <div className="mediaStyle">
           <ImageButton
             imageSrc={process.env.PUBLIC_URL + "/githublogo.png"}
             altText="Button Image"
-            onClick={handleMediaButton}
+            onClick={handleGithub}
           />
           <ImageButton
             imageSrc={process.env.PUBLIC_URL + "/devpostlogo.png"}
             altText="Button Image"
-            onClick={handleMediaButton}
+            onClick={handleDevpost}
           />
           <ImageButton
             imageSrc={process.env.PUBLIC_URL + "/youtubelogo.png"}
             altText="Button Image"
-            onClick={handleMediaButton}
+            onClick={handleYoutube}
           />
           <ImageButton
             imageSrc={process.env.PUBLIC_URL + "/instagramlogo.png"}
             altText="Button Image"
-            onClick={handleMediaButton}
+            onClick={handleInstagram}
           />
           <ImageButton
             imageSrc={process.env.PUBLIC_URL + "/linkedinlogo.png"}
             altText="Button Image"
-            onClick={handleMediaButton}
+            onClick={handleLinkedin}
           />
         </div>
 
         <div className="aboutMe">
           <img
-            class="avatar profilePosition"
+            className="avatar profilePosition"
             src={process.env.PUBLIC_URL + "/profilePicture.png"}
           ></img>
-          <strong class="spaceStyleHeading headingPosition">YUV BINDAL</strong>
-          <strong class="spaceStyleSubheading subheadingPosition">
+          <strong className="spaceStyleHeading headingPosition">YUV BINDAL</strong>
+          <strong className="spaceStyleSubheading subheadingPosition">
             AI enthusiast/ Technoprenuer
           </strong>
-          <strong class="spaceStyleText textPosition">
+          <strong className="spaceStyleText textPosition">
             Hey! I am Yuv, an aspiring technoprenuer. I am passionate about
             finding innovative solutions to global issues. I specialise in
             technologies such as <i>Computer Vision</i>, <i>Blockchain</i>, and{" "}
--- src/MyComponents/ImageButton.js
+++ src/MyComponents/ImageButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Background.css";
 
+
+
 const ImageButton = ({ imageSrc, altText, onClick }) => {
   return (
-    <button class='iconStyle' onClick={onClick}>
-      <img class='imageStyle' src={imageSrc} alt={altText} />
+    <button className='iconStyle' onClick={onClick}>
+      <img className='imageStyle' src={imageSrc} alt={altText} />
     </button>
   );
 };
